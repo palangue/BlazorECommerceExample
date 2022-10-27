fec78377dc241d9514a239f0d4b3e4085b8c9f23
uncleboksun <jwcho79@gmail.com>
2022-10-28 00:04:00 +0900

    ll

---
+++ BlazorECommersData/DemoProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BlazorECommersData
+{
+    public class DemoProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
+
+        public IEnumerable<DemoProductProp> ProductProperties { get; set; }
+    }
+}
+
+
+
---
+++ BlazorECommersData/DemoProductProp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BlazorECommersData
+{
+    public class DemoProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+    }
+}
+
